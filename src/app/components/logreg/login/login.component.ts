c9465b4503b328dba123124c09ccf3882503aea0
Guster <spavle.gh@gmail.com>
2024-09-10 21:19:41 +0200

    login komponenta template + validacija forme. Treba da se uradi autentifikacija pomocu store-a

--- src/app/components/logreg/login/login.component.ts
+++ src/app/components/logreg/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { checkErrors } from '../../../../helpers/validationErrorMessage';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: '../logreg.component.scss'
 })
 export class LoginComponent {
+  
+  constructor(
+    private userService: UserService
+  ){}
 
+  errorMsg: string | null = null;
+    loginForm = new FormGroup({
+    email: new FormControl<string>('', [
+      Validators.nullValidator,
+      Validators.required,
+      Validators.email,
+      Validators.maxLength(25)
+    ]),
+    password: new FormControl<string>('', [
+      Validators.nullValidator,
+      Validators.required,
+      Validators.maxLength(22),
+      
+    ])
+  }, [])
+
+
+  onSubmit(){
+    this.errorMsg = checkErrors(this.loginForm);
+    if(!this.errorMsg){
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+      
+     
+      
+    }
+  }
 }
