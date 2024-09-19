e6ea1b5b9bcd075fe1b8b9ef83ffbbe2bd3362da
Guster <spavle.gh@gmail.com>
2024-09-19 11:56:49 +0200

    izmenjena logika u resolveru, sada radi kako sto je planirano

--- src/app/resolvers/credentials.resolver.ts
+++ src/app/resolvers/credentials.resolver.ts
@@ -3,7 +3,7 @@ import { ResolveFn, Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { AppState } from '../store/app-state';
 import { checkToken } from '../store/auth/auth.actions';
-import { combineLatest, filter, map, of, ReplaySubject, switchMap, take, tap } from 'rxjs';
+import { combineLatest, filter, map, of, ReplaySubject, switchMap, take, takeUntil, tap, withLatestFrom } from 'rxjs';
 import { selectIsAuthenticated } from '../store/auth/auth.selector';
 
 export const credentialsResolver: ResolveFn<boolean> = (route, state) => {
@@ -12,71 +12,59 @@ export const credentialsResolver: ResolveFn<boolean> = (route, state) => {
   const router = inject(Router);
   
   console.log("from resolver");
-  //store.dispatch(checkToken());
 
-  //prvo selektujem feature za autentifikaciju i filtriram null vrednost
-  //zato sto je to inicijalno stanje
-  //(selekt koristim u pipe operatoru da bih mogao da dodam filter proveru  i take(1) u stream)
-  const isAuth$ = store.pipe(
-    select(selectIsAuthenticated),
-    filter((isAuth) => isAuth !== null)
-  );
+  store.dispatch(checkToken());
 
-  //vraca undefined kada se izvrsi dispatch (dakle sigurno se izvrsio dispatch)
-  const dispatch$ = of(store.dispatch(checkToken()));
-
-  //obezbedjuje da dispatch mora da se izvrsi
-  //emituje se vrednost tek kada oba observables-a emitiju nesto dakle dispatch se mora izvrsiti
-  //(dispatch ce emitovati undefined)
-  // dok ce isAuth$ emitovati vrednost koja nije null (dakle dispatch je sigurno zamenio stanje u store-u)
-  return combineLatest([isAuth$, dispatch$]).pipe(
-    switchMap(([isAuth]) => {
+  return store.select(selectIsAuthenticated).pipe(
+    filter(isAuth => isAuth !== null),
+    take(1),
+    map((isAuth) => {
       if(isAuth)
         router.navigate(['authenticated/home']);
       else
         router.navigate(['account/login']);
-      //obavlja se navigacija, uzima jedna vrednost
-      //i u svakom slucaju vraca true da bi se nastavilo sa inicijalizacijom
-      return store.pipe(
-        take(1),
-        filter((isAuth) => !isAuth)
-      )
+      return true;
     })
   )
   
 
 };
 
+// const store = inject(Store<AppState>);
+// const router = inject(Router);
 
-  // //prvo selektujem feature za autentifikaciju i filtriram null vrednost
-  // //zato sto je to inicijalno stanje
-  // //(selekt koristim u pipe operatoru da bih mogao da dodam filter proveru  i take(1) u stream)
-  // const isAuth$ = store.pipe(
-  //   select(selectIsAuthenticated),
-  //   filter((isAuth) => isAuth !== null)
-  // );
+// console.log("from resolver");
+// //store.dispatch(checkToken());
 
-  // //vraca undefined kada se izvrsi dispatch (dakle sigurno se izvrsio dispatch)
-  // const dispatch$ = of(store.dispatch(checkToken()));
+// //prvo selektujem feature za autentifikaciju i filtriram null vrednost
+// //zato sto je to inicijalno stanje
+// //(selekt koristim u pipe operatoru da bih mogao da dodam filter proveru  i take(1) u stream)
+// const isAuth$ = store.pipe(
+//   select(selectIsAuthenticated),
+//   filter((isAuth) => isAuth !== null)
+// );
 
-  // //obezbedjuje da dispatch mora da se izvrsi
-  // //emituje se vrednost tek kada oba observables-a emitiju nesto dakle dispatch se mora izvrsiti
-  // //(dispatch ce emitovati undefined)
-  // // dok ce isAuth$ emitovati vrednost koja nije null (dakle dispatch je sigurno zamenio stanje u store-u)
-  // return combineLatest([isAuth$, dispatch$]).pipe(
-  //   switchMap(([isAuth]) => {
-  //     if(isAuth)
-  //       router.navigate(['authenticated/home']);
-  //     else
-  //       router.navigate(['account/login']);
-  //     //obavlja se navigacija, uzima jedna vrednost
-  //     //i u svakom slucaju vraca true da bi se nastavilo sa inicijalizacijom
-  //     return store.pipe(
-  //       take(1),
-  //       filter((isAuth) => true)
-  //     )
-  //   })
-  // )
+// //vraca undefined kada se izvrsi dispatch (dakle sigurno se izvrsio dispatch)
+// const dispatch$ = of(store.dispatch(checkToken()));
+
+// //obezbedjuje da dispatch mora da se izvrsi
+// //emituje se vrednost tek kada oba observables-a emitiju nesto dakle dispatch se mora izvrsiti
+// //(dispatch ce emitovati undefined)
+// // dok ce isAuth$ emitovati vrednost koja nije null (dakle dispatch je sigurno zamenio stanje u store-u)
+// return combineLatest([isAuth$, dispatch$]).pipe(
+//   switchMap(([isAuth]) => {
+//     if(isAuth)
+//       router.navigate(['authenticated/home']);
+//     else
+//       router.navigate(['account/login']);
+//     //obavlja se navigacija, uzima jedna vrednost
+//     //i u svakom slucaju vraca true da bi se nastavilo sa inicijalizacijom
+//     return store.pipe(
+//       take(1),
+//       filter((isAuth) => !isAuth)
+//     )
+//   })
+// )
 
 
   //drugi pristup
